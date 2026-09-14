faaebc8a228da8b816d1beb424322763d7629117
steffli <stephan-liu@hotmail.de>
2026-09-14 22:39:48 +0200

    added generate and identify methods for pointer and reference

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,4 +3,53 @@
 #include  "C.hpp"
 #include  "Base.hpp"
 
+Base* generate(void){
+    int random_N = rand();
+    if (random_N % 3 == 0)
+        return new A;
+    else if (random_N % 3 == 1)
+        return new B;
+    else
+        return new C;
+}
 
+void identify(Base *p){
+    if (dynamic_cast<A*>(p))
+        std::cout << "A" << std::endl;
+    else if (dynamic_cast<B*>(p))
+        std::cout << "B" << std::endl;
+    else if (dynamic_cast<C*>(p))
+        std::cout << "C" << std::endl;
+}
+
+void identify(Base& p){
+    try{
+        A &a = dynamic_cast<A&>(p);
+        (void)a;
+        std::cout << "A" << std::endl;
+    }
+    catch{
+        std::cout << "Could not cast A" << std::endl;
+    }
+    try{
+        B &b = dynamic_cast<B&>(p);
+        (void)b;
+        std::cout << "B" << std::endl;
+    }
+    catch{
+        std::cout << "Could not cast B" << std::endl;
+    }
+    try{
+        C &c = dynamic_cast<C&>(p);
+        (void)c;
+        std::cout << "C" << std::endl;
+    }
+    catch{
+        std::cout << "Could not cast C" << std::endl;
+    }
+}
+
+int main(void)
+{
+
+}
